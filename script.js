30ea991fdd9a2382fed98f29d93cfc5be02b5c08
Estudiante LTIC <Estudiante@ltic.local>
2026-09-25 12:40:44 -0500

    Actualizar proyecto

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 class ServicioError extends Error {
-    constructor(mensaje) {
-        this.name = "ServicioError";
-    }
+  constructor(mensaje) {
+    super(mensaje);
+    this.name = "ServicioError";
+  }
 }
 
 function obtenerDatosUsuario() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const exito = Math.random() > 0.3;
-            if(existo) {
-                resolve({ usuario: "Ana", rol: "estudiante" });
-            }else {
-                reject(new ServicioError("No se pudo conectar con el servicio"));
-            }
-        },800);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const exito = Math.random() > 0.3;
+      if (exito) {
+        resolve({ usuario: "Ana", rol: "estudiante" });
+      } else {
+        reject(new ServicioError("No se pudo conectar con el servicio"));
+      }
+    }, 800);
+  });
 }
 
 const boton = document.getElementById("btnCargar");
 const resultado = document.getElementById("resultado");
 
 boton.addEventListener("click", async () => {
-    resultado.textContent = "Cargando...";
-    try {
-        const datos = await obtenerDatosUsuario();
-        resultado.textContent = `Bienvenido: ${datos.usuario}, Rol: ${datos.rol}`;
-    }catch (error) {
-        resultado.textContent = `Error: ${error.message}`;
-    } finally {
-        console.log("Intento de carga finalizado")
-    }
+  resultado.textContent = "Cargando...";
+  try {
+    const datos = await obtenerDatosUsuario();
+    resultado.textContent = `Bienvenido, ${datos.usuario} (${datos.rol})`;
+  } catch (error) {
+    resultado.textContent = `Error: ${error.message}`;
+  } finally {
+    console.log("Intento de carga finalizado");
+  }
 });
